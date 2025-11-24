ded2bcfb444b130a5fbc7038e9f4468029743577
rwaidaalmehanni <rwaida4@gmail.com>
2025-11-24 14:51:35 +0300

    remove local test file

--- test-local.js
+++
@@ -1,7 +0,0 @@
-const { hijriInputFormat, getCurrentHijriDate } = require('./dist/index.js');
-
-console.log('🧪 Testing hijri-date-formatter...');
-console.log('Current Hijri date:', getCurrentHijriDate());
-console.log('Arabic format:', hijriInputFormat(new Date(), 'ar', false));
-console.log('English format:', hijriInputFormat(new Date(), 'en', true));
-console.log('✅ All tests passed!');
